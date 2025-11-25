2604a5370ba059a40c890a0152943db35255ccb3
faluy <lyubashov.konstantin@mail.ru>
2025-11-25 09:06:39 +0300

    задание 12.py

--- задание 12.py
+++ задание 12.py
@@ -1,4 +1,9 @@
-number = [2, 4, 7, 1, 9]
+def find_index_builtin(arr, target):
+    try:
+        return arr.index(target)
+    except ValueError:
+        return -1
+arr = [2, 4, 7, 1, 9]
 target = 7
-print("Массив:", number)
-print(f"Число для поиска: {target}")
+result = find_index_builtin(arr, target)
+print(f"Индекс числа {target}: {result}") 
