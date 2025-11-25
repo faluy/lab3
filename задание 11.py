b788450407f943857ab54e43e5a31404c69af95b
faluy <lyubashov.konstantin@mail.ru>
2025-11-25 09:05:17 +0300

    задание 11.py

--- задание 11.py
+++ задание 11.py
@@ -1,2 +1,10 @@
-number = [5, 2, 8, 1, 9]
-print("Исходный массив:", number)
+def second_largest_sort(arr):
+    if len(arr) < 2:
+        return None
+    unique_sorted = sorted(set(arr))
+    if len(unique_sorted) < 2:
+        return None
+    return unique_sorted[-2]
+arr = [5, 2, 8, 1, 9]
+result = second_largest_sort(arr)
+print(f"Второй по величине элемент: {result}")  
