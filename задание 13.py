b29420f54003a4b01cc9d335d8b7fd4f7bed1ce7
faluy <lyubashov.konstantin@mail.ru>
2025-11-25 09:08:07 +0300

    задание 13.py

--- задание 13.py
+++ задание 13.py
@@ -1,2 +1,5 @@
-number = [10, 20, 30, 40, 50]
-print("Исходный массив:", number)
+def even_indices_slice(arr):
+    return arr[::2]
+arr = [10, 20, 30, 40, 50]
+result = even_indices_slice(arr)
+print(f"Элементы с четными индексами: {result}")
